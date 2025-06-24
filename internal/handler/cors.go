f192a4cf73a0c5e5924ae7b8f0c77e50d73a8a5c
BaGreal2 <artemonik17@gmail.com>
2025-06-24 23:20:54 +0200

    feat: Add patch to allow methods

--- internal/handler/cors.go
+++ internal/handler/cors.go
@@ -9,7 +9,7 @@ func WithCORS(next http.HandlerFunc) http.HandlerFunc {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Vary", "Origin")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
